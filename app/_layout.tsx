9e9b97ba41fc085d0adff05b39cf38e53b738887
Paula Joyce Ucol <peaksenut@gmail.com>
2025-03-06 20:30:29 +0800

    add: screens/routes with expo router

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,20 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{ title: "Welcome", headerShown: false }}
+      />
+      <Stack.Screen
+        name="character-selection"
+        options={{ title: "Choose a Character", headerShown: false }}
+      />
+      <Stack.Screen
+        name="chat/[character]"
+        options={{ title: "Chat", headerShown: false }}
+      />
+    </Stack>
+  );
 }
